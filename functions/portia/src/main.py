e07fee6f1f020f97cfa99b225d61cb1f7668ae88
Ayan G. <imluckyayan@gmail.com>
2025-08-19 21:22:48 +0530

    Init

---
+++ functions/portia/src/main.py
@@ -0,0 +1,57 @@
+from appwrite.client import Client
+from appwrite.services.users import Users
+from appwrite.exception import AppwriteException
+import os
+from portia import (
+    Config,
+    LLMProvider,
+    Portia,
+    example_tool_registry,
+)
+
+# This Appwrite function will be executed every time your function is triggered
+def main(context):
+    google_config = Config.from_default(
+        llm_provider=LLMProvider.GOOGLE,
+        default_model="google/gemini-2.0-flash",
+        google_api_key=context.res.text(os.environ['GOOGLE_API_KEY'])
+
+    )
+
+    portia = Portia(config=google_config, tools=example_tool_registry)
+    
+    req_path = context.req.path
+    if (req_path == "/ping"):
+        return context.res.text("Pong")
+    
+    if (req_path == "/add"):
+        plan_run = portia.run('add 1 + 2')
+        res = plan_run.model_dump_json(indent=2)
+        print(res)
+        return context.res.json(res)
+
+    return context.res.text("hello")
+
+
+    # try:
+    #     response = users.list()
+    #     # Log messages and errors to the Appwrite Console
+    #     # These logs won't be seen by your end users
+    #     context.log("Total users: " + str(response["total"]))
+    # except AppwriteException as err:
+    #     context.error("Could not list users: " + repr(err))
+
+    # # The req object contains the request data
+    # if context.req.path == "/ping":
+    #     # Use res object to respond with text(), json(), or binary()
+    #     # Don't forget to return a response!
+    #     return context.res.text("Pong")
+
+    # return context.res.json(
+    #     {
+    #         "motto": "Build like a team of hundreds_",
+    #         "learn": "https://appwrite.io/docs",
+    #         "connect": "https://appwrite.io/discord",
+    #         "getInspired": "https://builtwith.appwrite.io",
+    #     }
+    # )
